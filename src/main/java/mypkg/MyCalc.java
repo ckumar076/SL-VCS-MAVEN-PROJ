11f48285eef244e8cb72e95d0304eb1054f42d9a
Chandan Kumar <chandan.sap.76@outlook.com>
2026-01-31 14:38:59 +0530

    remove add

--- src/main/java/mypkg/MyCalc.java
+++ src/main/java/mypkg/MyCalc.java
@@ -1,11 +1,8 @@
 package mypkg;
 
 public class MyCalc {
-	public int sum(int a, int b)
-	{
-		return a+b;
-	}
-	public int diff(int a, int b)
+	
+    public int diff(int a, int b)
 	{
 		return a-b;
 	}
